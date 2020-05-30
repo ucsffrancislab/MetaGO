d9856ae52c31df8be0c7fba56f547da11e15c234
Jake <github@jakewendt.com>
2020-05-30 16:13:40 -0700

    Parallelized split_tupleData.py calls

--- MetaGO_SourceCode/split_tupleData.sh
+++ MetaGO_SourceCode/split_tupleData.sh
@@ -19,15 +19,15 @@ pieces=$2
 echo  $pieces
 mkdir splited_file
 cd temporary_files/
+echo $PWD
 for item2 in *.txt ; do
 	#echo $item2
 	#fileSize=`du -s $item2`
 	#echo $fileSize
-	echo $PWD
-	python ../../split_tupleData.py -s $pieces -f $item2 -p ../splited_file/
+	echo "python ../../split_tupleData.py -s $pieces -f $item2 -p ../splited_file/"
 	#python split_tupleData.py -s $pieces -f $item2 -p ../splited_file/
 	#rm $item2 -rf
-done
+done | parallel
 
 #mv *.txt *.py ../
 mv *.txt ../
